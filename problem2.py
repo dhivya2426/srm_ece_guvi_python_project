2456911a26a0cab39f91ea3634610616d43c3a17
dhivya2426 <dhivyashree2426@gmail.com>
2025-10-17 12:38:33 +0530

    Add files via upload

---
+++ problem2.py
@@ -0,0 +1,7 @@
+a=int(input("enter a number: "))
+if(a>=0 and a<=100):
+    print("the total bill=",a*5)
+elif(a>100 and a<=200):
+    print("the total bill=",a*7)
+else:
+    print("the total bill=",a*10) 
